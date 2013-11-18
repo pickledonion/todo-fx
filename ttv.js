a78a9029b8074881b13a533ed251e08131d73b33
pickledonion <onion@pickledonion.com>
2013-11-18 19:42:01 +0000

    ttv edit bugs

--- ttv.js
+++ ttv.js
@@ -1,6 +1,6 @@
 #! /usr/local/bin/rlwrap /usr/bin/jjs -fx -scripting
 //simple test 16
-/*global _, util, app, trace, main, loading, $SCRIPTS, fxml */
+/*global _, util, app, trace, main, loading, $SCRIPTS, fxml, prn, watch, repl */
 
 (function () {
   'use strict';
@@ -8,13 +8,15 @@
 
   root.app = {};
   app.init = function (state) {
-
-    state.data = state.data || {name: 'dog', desc:'woof', cost:23, children: [{name:'puppy', desc:'wif', cost:100}, {name:'cat', desc:'miaow', cost:10000 }]};
+    print('app.init');
+    state.data = state.data || app.data();
     state.tree = app.makeTree(state.data);
 
-    app.ttv = fxml.get('ttv');
+    // app.ttv = fxml.get('ttv');
+    app.ttv = new javafx.scene.control.TreeTableView();
+    app.ttv.id = 'ttv';
     app.ttv.root = state.tree;
-    // app.ttv = new javafx.scene.control.TreeTableView(state.tree);
+
     app.ttv.editable = true;
     state.tree.setExpanded(false);
     state.tree.setExpanded(true);
@@ -25,6 +27,7 @@
     _.each(app.colNames, function (key) {
       print('COL! :' + key);
       var col  = new javafx.scene.control.TreeTableColumn();
+      col.prefWidth = 150;
       col.cellValueFactory =
         new javafx.util.Callback({ call: function (obj) {
           return new javafx.beans.property.SimpleStringProperty(obj.value.value[key]);
@@ -32,41 +35,99 @@
       col.cellFactory =
         javafx.scene.control.cell.TextFieldTreeTableCell.forTreeTableColumn();
       app.ttv.columns.add(col);
+
+      col.onEditCancel = function (evt) {
+        print('oneditcancel');
+        // app.editCell(evt);
+        app.init(root.appState);
+      };
+
+      col.onEditStart = function (evt) { print('oneditstart');};
+
       col.onEditCommit = function (evt) {
-        print('EDIT!');
-        prn(evt.rowValue.value);
-        print(evt.treeTablePosition.column);
-        evt.rowValue.value[app.colNames[evt.treeTablePosition.column]] = evt.newValue;
-        state.tree.setExpanded(false);
-        state.tree.setExpanded(true);
-
+        app.editCell(evt);
       };
-      app.addButton = fxml.get('add');
-      app.addButton.onAction = function () {
-        print('add!');
-      };
-
+      fxml.get('add').onAction = app.addToSelected;
+      fxml.get('delete').onAction = app.deleteSelected;
     });
-    // fxml.openWindow();
-    // $STAGE.scene.root.children.add(app.ttv);
+
+    fxml.openWindow();
+
+    var fxttv = fxml.get('ttv');
+    if (fxttv) {fxttv.parent.children.removeAll(fxttv);}
+    $STAGE.scene.root.children.add(app.ttv);
+
   };
 
-  // app.findTreeItem = function (treeItem, match) {
-  //   var ary = util.javaToArray(treeItem.children),
-  //   res;
-  //   if(treeItem.value === match ) { print('hit'); return treeItem; }
-  //   if(_.isArray(ary)){
-  //     _.each(ary, function (child) {
-  //       res = app.findTreeItem(child, match);
-  //       if (res) { print('hitted'); return;}
-  //     });
-  //   }
-  //   return res;
-  // };
+  app.editCell = function (evt) {
+    print('EDIT!');
+    root.evt = evt;
+    prn(evt.rowValue && evt.rowValue.value);
+    print(evt.treeTablePosition.column);
+    print('new: ' + evt.newValue);
+    var idx = app.colNames[evt.treeTablePosition.column];
+    evt.rowValue.value[idx] = evt.newValue;
+    var treeItem = app.findTreeItem(app.ttv.root, evt.rowValue.value);
+    print('treeItem ' + treeItem );
+    if (treeItem) {
+      var parent = treeItem.parent;
+      prn(evt.rowValue.value);
+      print('treeItem: ' + treeItem)
+      app.init(root.appState);
+      // parent.setExpanded(false);
+      // parent.setExpanded(true);
+    }
+  };
 
-  app.add = function () {
-    var treeItem = app.findTreeItem(app.ttv.root, app.selected());
-    app.makeTree()
+  app.data = function () {
+    return {
+      name: 'outline', desc:'', cost:'', children:
+      [
+        {name:'puppy', desc:'wif', cost:100},
+        {name:'cat', desc:'miaow', cost:10000 }
+      ]
+    };
+  };
+
+  app.deleteSelected = function () {
+    print('in deleteSelected');
+    var item = app.selected();
+    prn(item);
+    if(!item) {return;}
+    app.deleteItem([root.appState.data], item);
+    app.init(root.appState);
+  };
+
+  app.deleteItem = function (context, match) {
+    var hit = -1;
+    _.each(context, function (obj, index) {
+      if (obj === match) { print('!HIT ' + index ); hit = index; }
+      else if (_.isArray(obj.children)) { app.deleteItem(obj.children, match); }
+    });
+    if (hit !== -1) { print('SPLICE!'); context.splice(hit, 1);}
+  };
+
+  app.addToSelected = function () {
+    var parent = app.selected() || app.ttv.root.value;
+    app.addItem(parent, {name:'-', desc:'-', cost:'-' });
+    app.init(root.appState);
+  };
+
+  app.addItem = function (parent, child) {
+    if(!parent.children) { parent.children = []; }
+    parent.children.push(child);
+  };
+
+  app.findTreeItem = function (treeItem, match) {
+    if(treeItem.value === match ) { return treeItem; }
+    var ary = util.javaToArray(treeItem.children);
+    if(_.isArray(ary)){
+      return  _.find(_.map(ary, function (child) {
+        return app.findTreeItem(child, match);
+      }), function (obj) {return obj !== false;});
+    } else {
+      return false;
+    }
   };
 
   app.selected = function () {
@@ -80,7 +141,7 @@
       app.makeTree(obj, treeItem);
     });
     if (dst) { dst.children.add(treeItem); }
-    else { return treeItem; };
+    else { return treeItem; }
   };
 
   root.appState = root.appState || {};
@@ -102,7 +163,7 @@
 
   root.replState = {later:true, filter:root.pretty.format};
 
-  root.timer && root.timer.stop();
+  if (root.timer) {root.timer.stop();}
   root.timer = new javafx.animation.AnimationTimer(function () {
     // app.update(root.appState);
     watch.update(root.watchState);
