a7fd0f7d4ecfd18c7a25e97ae73f97ee0ec8b6dd
pickledonion <onion@pickledonion.com>
2013-12-03 15:11:36 +0000

    added license

--- src/date-picker.js
+++ src/date-picker.js
@@ -1,4 +1,37 @@
 #! /usr/local/bin/rlwrap /usr/bin/jjs -fx -scripting
+
+  /*
+ * Copyright (c) 2013, Oracle and/or its affiliates.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * This file is available and licensed under the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the distribution.
+ *  - Neither the name of Oracle Corporation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 //hot dates: date picker example 01
 /*global _, util, app, trace, main, loading, $SCRIPTS, fxml, prn, watch, repl, app, loading */
 
@@ -37,7 +70,6 @@
       fxAry = javafx.collections.FXCollections.observableArrayList(javaAry);
     }
     list.items = fxAry;
-    list.items = fxAry;
   };
 
   app.setHandler = function (id, evt) {
@@ -54,8 +86,6 @@
     state.dates.splice(idx, 1);
     app.render(state);
     sel.clearSelection();
-    // var newWrapIdx = idx % state.list.
-    // sel.clearAndSelect();
   };
 
   app.addDate = function (state) {
